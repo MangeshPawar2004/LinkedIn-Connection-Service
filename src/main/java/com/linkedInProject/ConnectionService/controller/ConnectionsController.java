2a58c2af0bd37ec96ead90544a329c36a6a8afe9
MangeshPawar2004 <mangeshpawarmrp2004@gmail.com>
2026-09-12 23:13:16 +0530

    added Authntication

--- src/main/java/com/linkedInProject/ConnectionService/controller/ConnectionsController.java
+++ src/main/java/com/linkedInProject/ConnectionService/controller/ConnectionsController.java
@@ -3,14 +3,13 @@ package com.linkedInProject.ConnectionService.controller;
 import com.linkedInProject.ConnectionService.entity.Person;
 import com.linkedInProject.ConnectionService.service.ConnectionsService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/core")
 @RequiredArgsConstructor
